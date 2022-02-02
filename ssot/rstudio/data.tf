5650647d56e4f659ceb61ef4bc4b20f7c9dad28f
Rob Roos <robroos@git.daimler.com>
2022-02-02 13:50:31 +0100

    update data.tf remote provider

--- ssot/rstudio/data.tf
+++ ssot/rstudio/data.tf
@@ -2,9 +2,9 @@ data "terraform_remote_state" "bootstrap" {
   backend = "s3"
   config = {
     region         = "AWS_REGION"
-    bucket         = "PROJECT_NAME-bootstrap-state-ENVIRONMENT"
+    bucket         = "PROJECT_NAME-bootstrap-terraform-state-ENVIRONMENT"
     key            = "terraform.tfstate"
-    dynamodb_table = "PROJECT_NAME-bootstrap-state-lock-ENVIRONMENT"
+    dynamodb_table = "PROJECT_NAME-bootstrap-terraform-state-lock-ENVIRONMENT"
     encrypt        = false
   }
 }
